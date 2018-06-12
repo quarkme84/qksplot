ba56ef44c2de87850742ce71c0c26c9bfa147e83
Mihai Niculescu <q.quark@gmail.com>
2018-06-12 13:21:28 +0300

    moved/reparent images directory for the examples from hist

---
+++ doc/examples/hist/hist_proj.py
@@ -0,0 +1,38 @@
+import random as rnd
+import matplotlib.pyplot as plt
+
+from rootplots.hist import *
+from rootplots import mpl
+
+# create histograms
+h = Hist2D(30, -5, 5, 30, 0, 10, title="H2")
+
+# fill the histogram
+x, y = 0, 0
+for i in range(500000):
+    x = rnd.gauss(0,2)
+    y = 10 - rnd.expovariate(1./4.)
+    h.fill(x, y)
+
+projX = h.projectionX()
+projX.title = "Projection on X"
+
+projY = h.projectionY()
+projY.title = "Projection on Y"
+
+# create a grid where to rest the plots
+grid = plt.GridSpec(3, 1, wspace=0.2, hspace=0.4)
+
+# plot h
+plt.subplot(grid[0, 0])
+mpl.plot_hist(h)
+
+# plot projection on X
+plt.subplot(grid[1, 0])
+mpl.plot_hist(projX)
+
+# plot projection on Y
+plt.subplot(grid[2,0])
+mpl.plot_hist(projY)
+
+mpl.show()
